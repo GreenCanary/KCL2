342c155ed6414ce5fb9d6499095f2ac482de8c5e
misha <datenshiyohachi@gmail.com>
2025-04-08 15:43:36 +0300

    popout

--- src/main/java/com/mike/kcl2/Controller.java
+++
@@ -1,9 +0,0 @@
-package com.mike.kcl2;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class Controller {
-
-
-}
---
+++ src/main/java/com/mike/kcl2/MainController.java
@@ -0,0 +1,40 @@
+package com.mike.kcl2;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+public class MainController {
+
+    public void handleOpenWindow(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PopupWindow.fxml")); // your pop-up FXML
+            Parent root = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Pop-up Window");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void MenuInputPopOut(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PopoutInput.fxml")); // your pop-up FXML
+            Parent root = fxmlLoader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Pop-up Window");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
