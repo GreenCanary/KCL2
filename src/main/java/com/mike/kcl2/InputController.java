98ed727247bf35bf55b38b334f27ef1f368d8acd
GreenCanary <datenshiyohachi@gmail.com>
2025-08-26 12:25:50 +0300

    kcl3 final version contract model1

--- src/main/java/com/mike/kcl2/InputController.java
+++ src/main/java/com/mike/kcl2/InputController.java
@@ -34,9 +34,20 @@ public class InputController {
     @FXML private TextField densityTextField;
     @FXML private TextField redWaterTextField;
     @FXML private TextField techWaterTextField;
+
     @FXML private Label resultH2Ored;
     @FXML private Label resultH2Otech;
 
+    @FXML private Label agentSumLabel;
+    @FXML private Label vesovoyLabel;
+    @FXML private Label rashodLabel;
+
+    @FXML private Label calculatedAgentLabel;
+    @FXML private Label calculatedRedWaterLabel;
+    @FXML private Label correctionRedWaterLabel;
+    @FXML private Label calculatedTechWaterLabel;
+    @FXML private Label changeH2OLabel;
+    @FXML private Label neededRashodLabel;
 
 
     public void initData(InputValuesPrecentages inputValues) {
@@ -109,20 +120,21 @@ public class InputController {
             int temp = Integer.parseInt(tempTextField.getText());
             double density = Double.parseDouble(densityTextField.getText().replace(',', '.'));
             double mKek1 = Double.parseDouble(mKekTextField.getText().replace(',', '.'));
-            System.out.println("Agent : "+agent);
-            System.out.println("mKek1 : "+mKek1);
+            System.out.println("Agent : " + agent);
+            System.out.println("mKek1 : " + mKek1);
             double mKek = mKek1;
             double moist = 0.055;
             double slRatio = 0.8;
             double mTotal = 396.0;
 
-            double mLiquid = (mTotal * slRatio)/(slRatio + 1);
+
+            double mLiquid = (mTotal * slRatio) / (slRatio + 1);
             double mSolid = mTotal - mLiquid;
             double s_KCl_p = 90.0;
             double s_NaCl_p = 7.0;
             double s_CaSO4_p = 3.0;
 
-            if (temp != 20 && temp !=25 && temp !=30) {
+            if (temp != 20 && temp != 25 && temp != 30) {
                 // Create an alert to show the error message
                 Alert alert = new Alert(Alert.AlertType.ERROR,
                         "Ошибка: введена неверная температура! Допустимые значения: 20, 25, 30.",
@@ -175,17 +187,15 @@ public class InputController {
             double caso4Percentage = 0.4;
             double waterPercentage = (100 - (naclPercentage + kclPercentage + caso4Percentage));
             double waterPercentageMass = 0;
-            if (temp == 20){
-                 waterPercentageMass = 70.58;
+            if (temp == 20) {
+                waterPercentageMass = 70.58;
             }
-            if (temp == 25){
+            if (temp == 25) {
                 waterPercentageMass = 70.21;
             }
-            if (temp == 30){
+            if (temp == 30) {
                 waterPercentageMass = 69.51;
             }
-
-
 
 
             // Set the values
@@ -203,7 +213,7 @@ public class InputController {
             inputValues.setDensity(density);
             inputValues.setTemperature(temp);
 
-            System.out.println("kek: "+mKek);
+            System.out.println("kek: " + mKek);
 
             floto.setL_KCl_p(12);
             floto.setL_NaCl_p(20);
@@ -214,60 +224,56 @@ public class InputController {
             floto.setS_NaCl_p(7);
             floto.setS_CaSO4_p(3);
 
-            floto.setS_Check_v(Double.parseDouble(String.format("%.2f", mKek*1.15)));
-            floto.setS_CaSO4_v(Double.parseDouble(String.format("%.2f", (floto.getS_Check_v()/100)* floto.getS_CaSO4_p())));
-            floto.setS_NaCl_v(Double.parseDouble(String.format("%.2f", (floto.getS_Check_v()/100)* floto.getS_NaCl_p())));
-            floto.setS_KCl_v(Double.parseDouble(String.format("%.2f", (floto.getS_Check_v()/100)* floto.getS_KCl_p())));
+            floto.setS_Check_v(Double.parseDouble(String.format("%.2f", mKek * 1.15)));
+            floto.setS_CaSO4_v(Double.parseDouble(String.format("%.2f", (floto.getS_Check_v() / 100) * floto.getS_CaSO4_p())));
+            floto.setS_NaCl_v(Double.parseDouble(String.format("%.2f", (floto.getS_Check_v() / 100) * floto.getS_NaCl_p())));
+            floto.setS_KCl_v(Double.parseDouble(String.format("%.2f", (floto.getS_Check_v() / 100) * floto.getS_KCl_p())));
 
-            System.out.println("floto SCaSO4: "+floto.getS_CaSO4_v());
-            System.out.println("floto SNaCl: "+floto.getS_NaCl_v());
-            System.out.println("floto SKCl: "+floto.getS_KCl_v());
-            System.out.println("floto Scheck: "+floto.getS_Check_v());
+            System.out.println("floto SCaSO4: " + floto.getS_CaSO4_v());
+            System.out.println("floto SNaCl: " + floto.getS_NaCl_v());
+            System.out.println("floto SKCl: " + floto.getS_KCl_v());
+            System.out.println("floto Scheck: " + floto.getS_Check_v());
             System.out.println(" ");
 
-            floto.setL_Check_v(Double.parseDouble(String.format("%.2f", floto.getS_Check_v()*0.8)));
-            floto.setH2O_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v()/100)* floto.getH2O_p())));
-            floto.setL_CaSO4_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v()/100)* floto.getL_CaSO4_p())));
-            floto.setL_NaCl_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v()/100)* floto.getL_NaCl_p())));
-            floto.setL_KCl_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v()/100)* floto.getL_KCl_p())));
+            floto.setL_Check_v(Double.parseDouble(String.format("%.2f", floto.getS_Check_v() * 0.8)));
+            floto.setH2O_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v() / 100) * floto.getH2O_p())));
+            floto.setL_CaSO4_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v() / 100) * floto.getL_CaSO4_p())));
+            floto.setL_NaCl_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v() / 100) * floto.getL_NaCl_p())));
+            floto.setL_KCl_v(Double.parseDouble(String.format("%.2f", (floto.getL_Check_v() / 100) * floto.getL_KCl_p())));
 
-            System.out.println("floto H2O: "+floto.getH2O_v());
-            System.out.println("floto LCaSO4: "+floto.getL_CaSO4_v());
-            System.out.println("floto LNaCl: "+floto.getL_NaCl_v());
-            System.out.println("floto LKCl: "+floto.getL_KCl_v());
-            System.out.println("floto Lcheck: "+floto.getL_Check_v());
+            System.out.println("floto H2O: " + floto.getH2O_v());
+            System.out.println("floto LCaSO4: " + floto.getL_CaSO4_v());
+            System.out.println("floto LNaCl: " + floto.getL_NaCl_v());
+            System.out.println("floto LKCl: " + floto.getL_KCl_v());
+            System.out.println("floto Lcheck: " + floto.getL_Check_v());
             System.out.println(" ");
 
 
-
-
-
-            posleVish.setL_NaCl_p((Double.parseDouble(String.format("%.2f",naclPercentage))));  // Set NaCl percentage from lookup
+            posleVish.setL_NaCl_p((Double.parseDouble(String.format("%.2f", naclPercentage))));  // Set NaCl percentage from lookup
             posleVish.setL_KCl_p((Double.parseDouble(String.format("%.2f", kclPercentage))));    // Set KCl percentage from lookup
             posleVish.setL_CaSO4_p(0.4);
             double waterPercentagePosleVish = 100 - (posleVish.getL_NaCl_p() + posleVish.getL_KCl_p() + 0.4);
-            posleVish.setH2O_p((Double.parseDouble(String.format("%.2f",waterPercentagePosleVish))));
+            posleVish.setH2O_p((Double.parseDouble(String.format("%.2f", waterPercentagePosleVish))));
             posleVish.setL_Check_p(100);
 
             double waterTotal = floto.getH2O_v() + agent;
-            double Na = naclPercentage* (waterTotal/waterPercentage);
-            double onePercentWater = Na/16.9;
+            double Na = naclPercentage * (waterTotal / waterPercentage);
+            double onePercentWater = Na / 16.9;
             double waterTotalCalculated = onePercentWater * waterPercentageMass;
             double waterCorrection = waterTotalCalculated - waterTotal;
             double waterCorrectionCalculated = (agent + waterCorrection);
 
 
-
             posleVish.setH2O_v(Double.parseDouble(String.format("%.2f", floto.getH2O_v())));
-            posleVish.setL_KCl_v(Double.parseDouble(String.format("%.2f", result.getL_KCl_p()* result.getH2O_v()/ result.getH2O_p()+result.getS_NaCl_v())));
-            posleVish.setL_NaCl_v(Double.parseDouble(String.format("%.2f", floto.getS_NaCl_v()+floto.getL_NaCl_v()+redWater.getL_NaCl_v()- result.getS_NaCl_v())));
-            posleVish.setL_CaSO4_v(Double.parseDouble(String.format("%.2f", 0.4 * result.getH2O_v()/result.getH2O_p())));
+            posleVish.setL_KCl_v(Double.parseDouble(String.format("%.2f", result.getL_KCl_p() * result.getH2O_v() / result.getH2O_p() + result.getS_NaCl_v())));
+            posleVish.setL_NaCl_v(Double.parseDouble(String.format("%.2f", floto.getS_NaCl_v() + floto.getL_NaCl_v() + redWater.getL_NaCl_v() - result.getS_NaCl_v())));
+            posleVish.setL_CaSO4_v(Double.parseDouble(String.format("%.2f", 0.4 * result.getH2O_v() / result.getH2O_p())));
             posleVish.setL_Check_v(Double.parseDouble(String.format("%.2f", posleVish.getH2O_v() + posleVish.getL_KCl_v() + posleVish.getL_NaCl_v() + posleVish.getL_CaSO4_v())));
 
             posleVish.setS_KCl_v(Double.parseDouble(String.format("%.2f", floto.getS_KCl_v() + floto.getL_KCl_v() + redWater.getL_KCl_v() - result.getL_KCl_v())));
             posleVish.setS_CaSO4_v(Double.parseDouble(String.format("%.2f", floto.getS_CaSO4_v() + floto.getL_CaSO4_v() + redWater.getL_CaSO4_v() - result.getL_CaSO4_v())));
             posleVish.setS_Check_v(Double.parseDouble(String.format("%.2f", result.getS_KCl_v() + result.getS_NaCl_v() + result.getS_CaSO4_v())));
-            posleVish.setS_NaCl_v(Double.parseDouble(String.format("%.2f", floto.getS_KCl_v()/100 * 0.68)));
+            posleVish.setS_NaCl_v(Double.parseDouble(String.format("%.2f", floto.getS_KCl_v() / 100 * 0.68)));
 
             posleVish.setS_KCl_p(Double.parseDouble(String.format("%.2f", (result.getS_KCl_v() / result.getS_Check_v() * 100))));
             posleVish.setS_NaCl_p(Double.parseDouble(String.format("%.2f", (result.getS_NaCl_v() / result.getS_Check_v() * 100))));
@@ -275,10 +281,8 @@ public class InputController {
             posleVish.setS_Check_p(100);
 
 
-
-
-            System.out.println("KCl: "+posleVish.getL_KCl_p()+" %");
-            System.out.println("NaCl: "+posleVish.getL_NaCl_p()+" %");
+            System.out.println("KCl: " + posleVish.getL_KCl_p() + " %");
+            System.out.println("NaCl: " + posleVish.getL_NaCl_p() + " %");
             System.out.println("NaCl: " + posleVish.getL_NaCl_v());
 
 
@@ -292,23 +296,29 @@ public class InputController {
 
             double redWaterCorrectionCalculated = 0;
             double techWaterCorrectionCalculated = 0;
+            double resultCorrection = agent + waterCorrection;
 
-            if (waterTotalCalculated/1.04 > redWater1){
+            if (resultCorrection / 1.04 > redWater1) {
                 redWaterCorrectionCalculated = redWater1;
+            } else {
+                redWaterCorrectionCalculated = resultCorrection / 1.04;
             }
-            else {
-                redWaterCorrectionCalculated = waterTotalCalculated/1.04;
-            }
 
-            techWaterCorrectionCalculated = (waterCorrectionCalculated/1.04) - redWaterCorrectionCalculated;
-            setLabelValue(resultH2Ored, (Double.parseDouble(String.format("%.0f",redWaterCorrectionCalculated))));
-            setLabelValue(resultH2Otech, (Double.parseDouble(String.format("%.0f",techWaterCorrectionCalculated))));
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/mike/kcl2/Main.fxml")); // Adjust path to match your project structure
-            Parent root = loader.load();
+            techWaterCorrectionCalculated = (resultCorrection / 1.04) - redWaterCorrectionCalculated;
+            setLabelValue(resultH2Ored, (Double.parseDouble(String.format("%.2f", redWaterCorrectionCalculated))));
+            setLabelValue(resultH2Otech, (Double.parseDouble(String.format("%.2f", techWaterCorrectionCalculated))));
 
-            // Get the controller and pass the data
-            Application.getRootContainer().getChildren().setAll(root);
+            setLabelValue(agentSumLabel, (Double.parseDouble(String.format("%.2f", agent / 1.04))));
+            setLabelValue(vesovoyLabel, (Double.parseDouble(String.format("%.2f", mKek))));
+            double rashod = agent / mKek;
+            setLabelValue(rashodLabel, (Double.parseDouble(String.format("%.2f", rashod))));
 
+            setLabelValue(calculatedAgentLabel, (Double.parseDouble(String.format("%.2f", waterCorrectionCalculated / 1.04))));
+            setLabelValue(calculatedRedWaterLabel, (Double.parseDouble(String.format("%.2f", redWaterCorrectionCalculated))));
+            setLabelValue(correctionRedWaterLabel, (Double.parseDouble(String.format("%.2f", redWaterCorrectionCalculated - redWater1))));
+            setLabelValue(calculatedTechWaterLabel, (Double.parseDouble(String.format("%.2f", waterCorrectionCalculated / 1.04 - redWaterCorrectionCalculated))));
+            setLabelValue(changeH2OLabel, (Double.parseDouble(String.format("%.2f", techWaterCorrectionCalculated - techWater1))));
+            setLabelValue(neededRashodLabel, (Double.parseDouble(String.format("%.2f", (waterCorrectionCalculated / 1.04) / mKek))));
 
 
         } catch (NumberFormatException e) {
@@ -317,12 +327,9 @@ public class InputController {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Ошибка ввода: убедитесь, что все поля заполнены числами.", ButtonType.OK);
             alert.setTitle("Ошибка ввода");
             alert.showAndWait();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
         }
 
     }
-
-
 }
 
